a6e5b30455d76ca4d969de9a08848817c541a9b3
mustafabulu <you@example.com>
2022-02-18 14:37:36 +0300

    Bean

--- src/main/java/com/innova/beantuto/BeanController.java
+++ src/main/java/com/innova/beantuto/BeanController.java
@@ -1,5 +1,4 @@
 package com.innova.beantuto;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +13,12 @@ public class BeanController {
     @Autowired
     _02_BeanScope beanScope;
 
+    @Autowired
+    _03_BeanIntialDestroy beanIntialDestroy1;
+
+    @Autowired
+    _03_BeanIntialDestroy beanIntialDestroy2;
+
     // http://localhost:8080/bean/basic
     @GetMapping("/bean/basic")
     @ResponseBody
@@ -21,12 +26,18 @@ public class BeanController {
         return beanBasic.beanBasic()+"";
     }
 
-
     // http://localhost:8080/bean/scope
     @GetMapping("/bean/scope")
     @ResponseBody
     public String getBeanScope(){
         return beanBasic.beanBasic().hashCode()+" ??? "+beanScope.beanScope().hashCode();
+    }
+
+    // http://localhost:8080/bean/initial/destroy
+    @GetMapping("/bean/initial/destroy")
+    @ResponseBody
+    public String getBeanInitialDestroy(){
+        return beanIntialDestroy1.beanInitial().hashCode()+" ??? "+beanIntialDestroy2.beanInitial().hashCode();
     }
 
 }
